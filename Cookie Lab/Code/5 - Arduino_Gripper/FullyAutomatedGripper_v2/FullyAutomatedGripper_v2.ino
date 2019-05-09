ff3b765b56950c93dd6860dea9f61eb6dcbb0979
Calvin <calvinbrice@earthlink.net>
2019-05-08 22:25:48 -0500

    Update FullyAutomatedGripper_v2.ino

--- Cookie Lab/Code/5 - Arduino_Gripper/FullyAutomatedGripper_v2/FullyAutomatedGripper_v2.ino
+++ Cookie Lab/Code/5 - Arduino_Gripper/FullyAutomatedGripper_v2/FullyAutomatedGripper_v2.ino
@@ -21,41 +21,43 @@ const byte MY_ADDRESS = 3;  // i2c address of board
 
 // pin assignments
 int LED_PIN = 13;
-int MOTOR_STEP_PIN = 3; // --> PUL+
-int MOTOR_DIRECTION_PIN = 4; // --> DIR+
-int LIMIT_SWITCH_PIN = 5;// --> Limit switch RED wire
+int MOTOR_STEP_PIN = 6;       // --> PUL+
+int MOTOR_DIRECTION_PIN = 4;  // --> DIR+
+int LIMIT_SWITCH_PIN = 8;     // --> Limit switch RED wire
 int FINGER_SERVO = 9;
 int WRIST_SERVO = 10;
-int ROTATION_SERVO = 11;
-
-//Rewrite so that these are stored in an array... or a struct
-//this will allow for easier access and less variables to keep track of
-
+//int ROTATION_SERVO = 11;
 
 // Structs
-struct armElement {
-  float set; //sets location of future position
-  float current; // holds current positional value
-  int8_t minmax[2]; // limit parameters
-}
+typedef struct {
+  float pos;
+  float cur;
+  float minimum;
+  float maximum;
+} arm;
 
-//Positional input parameters and initialization (pos = position)
-int finger_pos = 0;     // 0~180 deg
-int wrist_pos = 0;      // 0~180 deg
-int height_pos = 0;     // 0~400 mm
-int rotation_pos = 0;   // 0~?   deg
+arm finger = {0, 0, 0, 180};    //initializes a finger under the arm type with (pos, cur, minimum, maximum)
+arm wrist = {0, 0, 0, 180};     //wrist is limited by wiring length... do not mess with these values or you risk damage
+arm height = {0, 0, 0, 370};    //maximum travel of linear actuator is 400mm - the height of the limit switch (30mm)
+arm rotation = {0, 0, 0, 180};  //
 
-//Positional tracking parameters (cur = current)
-int finger_cur = finger_pos;       // 0~180 deg
-int wrist_cur = wrist_pos;         // 0~180 deg
-int height_cur = height_pos;       // 0~400 mm
-int rotation_cur = rotation_pos;   // 0~?   deg
-
-//Positional range parameters (min/max)
-int finger_par[] = {0, 180};       // 0~180 deg
-int wrist_par[] = {6, 107};        // 0~180 deg
-int height_par[] = {0, 400};       // 0~400 mm
-int rotation_par[] = {0, 180};     // 0~?   deg
+////Positional input parameters and initialization (pos = position)
+//int finger_pos = 0;     // 0~180 deg
+//int wrist_pos = 0;      // 0~180 deg
+//int height_pos = 0;     // 0~400 mm
+//int rotation_pos = 0;   // 0~?   deg
+//
+////Positional tracking parameters (cur = current)
+//int finger_cur = finger_pos;       // 0~180 deg
+//int wrist_cur = wrist_pos;         // 0~180 deg
+//int height_cur = height_pos;       // 0~400 mm
+//int rotation_cur = rotation_pos;   // 0~?   deg
+//
+////Positional range parameters (min/max)
+//int finger_par[] = {0, 180};       // 0~180 deg
+//int wrist_par[] = {6, 107};        // 0~180 deg
+//int height_par[] = {0, 400};       // 0~400 mm
+//int rotation_par[] = {0, 180};     // 0~?   deg
 
 Servo servo;  // create servo object to control a servo
 SpeedyStepper stepper; // create the stepper motor object
@@ -87,10 +89,10 @@ void setup()
 
 
   //Begin homing procedure
-  //setHome();
+  setHome();
 
-  Debug();
-  //setHeight(250);
+  //Debug();
+  setHeight(250);
   //Motion(finger_pos, wrist_pos, height_pos, rotation_pos);
 }
 
@@ -99,11 +101,19 @@ void loop()
   //State machine
   //  (actually the state machine should be on the master arduino...
   //  this one should only be receiving movement commmands and interpreting them)
-
+  //
+  //  Serial.print("finger.minimum = ");
+  //  Serial.println(finger.minimum);
+  //    Serial.print("finger.maximum = ");
+  //  Serial.println(finger.maximum);
+  //  Serial.println("");
+  //  delay(5000);
 
 }
 
 void motion(int finger_pos, int wrist_pos, int height_pos, int rotation_pos) {
   // Finger
-  if ( == finger_pos && finger_pos == d)
+  if ( finger.minimum <= finger.pos && finger.pos <= finger.maximum) {
+
+  }
 }
